139a6ef58926de44ca5fedf2057fd1358cc6ec99
metallizard <aditya.pratama2910@gmail.com>
2021-05-29 10:53:42 +0700

    Initial Commit

---
+++ Assets/FlappyBird/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public void Flap()
+    {
+
+    }
+
+    public void FallDown()
+    {
+
+    }
+
+    public void Dead()
+    {
+        
+    }
+}
---
+++ Assets/FlappyBird/Scripts/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public void OnPassingPipe()
+    {
+
+    }
+
+    public void AddScore()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
